16e7458bc6359f1feb0e8a92add84e04fd5d70c4
Pooja <poojapjan22@gmail.com>
2026-05-28 21:50:59 +0530

    Added User Model, UserService and UserController

--- src/main/java/com/eduhub/eduhub_backend/Exception/ResourceNotFoundException.java
+++ src/main/java/com/eduhub/eduhub_backend/Exception/ResourceNotFoundException.java
@@ -2,7 +2,11 @@ package com.eduhub.eduhub_backend.Exception;
 
 //Custom Exception
 public class ResourceNotFoundException extends RuntimeException {
-        public ResourceNotFoundException(String resource, String field, String error) {
+        public ResourceNotFoundException(
+                String resource,
+                String field,
+                String error) {
+
             super(String.format(
                     "%s not found with %s : %s",
                     resource,
